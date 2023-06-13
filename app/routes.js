5d425ddaf01f4db61f58740e6aa959e58c776b17
Annelies11 <mmahmudi1103@gmail.com>
2023-06-13 20:02:24 +0700

    first-commit

---
+++ app/routes.js
@@ -0,0 +1,17 @@
+const routes = [
+    {
+        method: 'GET',
+        path: '/',
+        handler: () => {
+            return 'Ini adalah halaman home hrrr'
+        }
+    },
+    {
+        method: '*',
+        path: '/',
+        handler: () => {
+            return 'Halaman ini tidak dapat diakses dengan method ini'
+        }
+    }
+]
+module.exports = routes
